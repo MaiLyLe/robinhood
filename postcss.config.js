54c1c41569c35f751a7b29193f6d621d29e5a7b4
Mai Ly Le <mailyle@MBP-von-Mai.fritz.box>
2021-07-03 17:50:18 +0200

    changes postcss config to add autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,6 @@
-// const tailwindcss = require('tailwindcss')
-
-// module.exports = {
-//     plugins: [
-//         require('postcss-easy-import'),
-//         tailwindcss('./tailwind.config.js'),
-//         require('autoprefixer'),
-//     ],
-// }
-
 module.exports = {
     plugins: {
-        'postcss-easy-import': {},
         tailwindcss: {},
+        autoprefixer: {},
     },
 }
--- src/elements/Logo/styles.ts
+++ src/elements/Logo/styles.ts
@@ -1,9 +1,10 @@
 import tw, { styled } from 'twin.macro'
 
 export const Logo = styled.div<{ isHeaderDark?: boolean }>`
+    height: 70px;
     svg {
         width: 200px;
-        height: 200px;
+        height: 70px;
     }
 
     // svg:hover {
